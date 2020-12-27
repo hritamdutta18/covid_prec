e5924fbcc5e53d5e6a9ddd9a53d7d4170f46efd0
Hritam Dutta <hritamloyola10@gmail.com>
2020-12-27 18:18:36 +0530

    JavaScript

--- js/pagejs.js
+++ js/pagejs.js
@@ -2,3 +2,14 @@ function displaymsg(){
     document.getElementById("success").style.display = "block";
     document.getElementById("contact-form").style.display = "none";
 }
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("header-nav").style.top = "0";
+  } else {
+    document.getElementById("header-nav").style.top = "-20%";
+  }
+  prevScrollpos = currentScrollPos;
+}
